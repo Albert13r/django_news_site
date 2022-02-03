7dc9728dfe2976348585a41d4b3ad710a33f5e36
albert <bet123098@gmail.com>
2022-02-03 19:25:53 +0200

    Added views news

--- news/models.py
+++ news/models.py
@@ -10,6 +10,7 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Image', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Publication status')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"pk": self.pk})
