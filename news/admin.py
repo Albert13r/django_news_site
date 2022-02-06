0a6ff001f9b7ccc6c33e3b1ee19c218f5fcb91d1
albert <bet123098@gmail.com>
2022-02-06 19:07:32 +0200

    Customized admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'category', 'title', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+
+    fields = (
+        'id', 'title', 'category', 'content', 'photo', 'get_photo', 'created_at', 'updated_at', 'views',
+        'is_published')
+    readonly_fields = ('get_photo', 'created_at', 'updated_at', 'views', 'id')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        else:
+            return 'Not found'
+
+    get_photo.short_description = 'photo'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +33,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.site_title = 'Sport News Settings'
+admin.site.site_header = 'Sport News Settings'
