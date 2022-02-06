e1c146103fbb9bc63de599435e39d386cce28342
albert <bet123098@gmail.com>
2022-02-06 17:17:51 +0200

    Added debug_toolbar

--- my_django_sites/urls.py
+++ my_django_sites/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
@@ -26,4 +25,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
